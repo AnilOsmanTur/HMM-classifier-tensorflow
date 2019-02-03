c824eac26762124c1265c3f883523dec729d8d78
Anıl Osman Tur <aaotrr@gmail.com>
2019-02-03 21:15:56 +0300

    tensor performance update
    performance update vectorized scoring and training modification

--- HMM_classifier.py
+++ HMM_classifier.py
@@ -13,22 +13,35 @@ np.random.seed(42)
 
 class HMM_classifier():
     ''' classifier with HMM models '''
-    def __init__(self, n_class=3, n_state=8, n_fea=7, n_iter=50, verbose=True):
+    def __init__(self, n_class=3, n_state=8, n_fea=7,
+                 stop_tol=0.001, min_variance=0.1, num_runs=1
+                 max_iter=100, batch_size=None, verbose=True, hmm_type='fully-connected'):
+        #hmm types (fully-connected, left-to-right, cyclic)
         self.n_class=n_class
-        self.n_iter = n_iter
+        self.max_iter = max_iter
+        self.stop_tol = stop_tol
+        self.min_var = min_variance
+        self.batch_size = batch_size,
+        self.n_runs = num_runs
         self.models = []
         for i in range(n_class):
             # create a model with 10 hidden states with features.
-            model = HMM(n_state, n_fea)
+            model = HMM(n_state, n_fea, hmm_type=hmm_type)
             self.models.append(model)
         
         self.trained = False
         
     def train(self, samples, labels):
         print('Training started')
-        for k in range(self.n_class):
+        for label in range(self.n_class):
             print('model class: {} training...'.format(k))
-            self.models[k].fit(samples[:,k], num_runs=self.n_iter)
+            idx_label = labels == label
+            self.models[label].fit(samples[idx_label],
+                                   max_steps=self.max_iter,
+                                   batch_size=self.batch_size,
+                                   TOL=self.stop_tol,
+                                   min_var=self.min_var,
+                                   num_runs=self.n_runs)
         print('Training finished')
         self.trained = True
             
@@ -37,18 +50,15 @@ class HMM_classifier():
         print('scoring...')
         size = samples.shape[0]
         scores = np.zeros((size, self.n_class), dtype=np.float32)
-        for k in range(size):
-            print('\r{:6.2f}%({}/{})'.format(100*(k+1)/size, k+1, size), end='')
-            for i in range(self.n_class):
-                scores[k,i] = self.models[i].posterior(samples[k,i])
+        for k in range(self.n_class):
+            print('\r{:6.2f}%({}/{})'.format(100*(k+1)/self.n_class, k+1, self.n_class), end='')
+            scores[:,i] = self.models[i].posterior(samples)
         print('\nscoring finished.')
         return scores
     
     def predict_samples(self, samples):
         scores = self.score_samples(samples)
-        
         pred = np.argmax(scores, axis=-1)
-        
         return pred
     
     def calculate_accuracy(self, pred, labels):
